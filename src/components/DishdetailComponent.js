ab80e9c8dfa65b6013c76dcb3fcda6054c5cec65
Alex Nassif <alexnassif@Alexs-MBP.home>
2018-08-07 18:28:39 -0700

    2 prep

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,51 +9,49 @@ class DishDetail extends Component {
 
   }
   renderComments(comments){
-    if(comments != null){
-      const c = comments.map((comment) => {
-          return (
-            <li>
-              <p>{comment.comment}</p>
-              <p>-- {comment.author}, {comment.date}</p>
-            </li>
-          );
-      });
+      if(comments != null){
+        const c = comments.map((comment) => {
+            return (
+              <li>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+              </li>
+            );
+        });
 
-      return (
-        <div className="container">
-        <h4>Comments</h4>
-          <ul className="list-unstyled">
-            {c}
-          </ul>
-        </div>
-      );
-  }
-  else{
-      return(
-        <div></div>
-      );
-  }
+        return (
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {c}
+            </ul>
+          </div>
 
-
+        );
+    }
+    else{
+        return(
+          <div></div>
+        );
+    }
   }
   render() {
     if(this.props.dishdetail != null){
     return(
-
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={this.props.dishdetail.image} alt={this.props.dishdetail.name} />
-            <CardBody>
-              <CardTitle>{this.props.dishdetail.name}</CardTitle>
-              <CardText>{this.props.dishdetail.description}</CardText>
-            </CardBody>
-          </Card>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              <Card>
+                <CardImg width="100%" src={this.props.dishdetail.image} alt={this.props.dishdetail.name} />
+                <CardBody>
+                  <CardTitle>{this.props.dishdetail.name}</CardTitle>
+                  <CardText>{this.props.dishdetail.description}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            {this.renderComments(this.props.dishdetail.comments)}
+          </div>
         </div>
-        <div className="col-12 col-md-5 m-1">
-          {this.renderComments(this.props.dishdetail.comments)}
-        </div>
-      </div>
 
     );
   }
